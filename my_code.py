eb39f4d3527985749a5f1d2f975983e2dad3c7f0
audmchoi <161496706+audmchoi@users.noreply.github.com>
2024-03-27 22:50:09 -0500

    Create my_code.py

---
+++ my_code.py
@@ -0,0 +1,11 @@
+import tkinter as tk 
+from tkinter import messagebox
+
+#This is going to be where I put the personality test questions, but for now Im just going to keep it as 1 question
+movie_recommendations = {
+  "action": ["Die Hard", "Avengers", "John Wick"]
+  "comedy": ["The Hangover", "Ferris Bueller's Day Off"]
+  "romance": ["The Notebook", "Pretty Woman"]
+# There will be a file associated with each of the genres that contains a large list of movies and answers to questions
+}
+
