12da0d42960b5768dc5962bb1b43005e454c5fb4
audmchoi <161496706+audmchoi@users.noreply.github.com>
2024-05-03 15:22:20 -0500

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -3,69 +3,64 @@ from tkinter import messagebox
 import random
 
 
-def load_movies(action):
+def load_movies(genre):
   try:
-    with open (f"{action}.txt, "r") as file: 
+    with open (f"{genre}.txt, "r") as file: 
                moives = file.readlines()
     movies = [movie.strop() for movie in movies]
     return movies 
+  except FileNotFoundError:
+    return []
+def load_movie_rec(genre_red):
+  try:
+    with open (f"{genre_rec}_rec.txt, "r") as file:
+               movies = [line.strip() for line in file.readlines()]
+    return movies
+  except FileNotFoundError:
+    return[]
+def load_actors(genre_actor):
+  try:
+    with open(f"{genre_actor}_actor.txt, "r") as file:
+              actors = [line.strip() for line in file.readlines()]
+    return actors
   except FileNotFoundError:
     return []
 
-def load_movies(romance):
-  try:
-    with open (f"{romance}.txt, "r") as file: 
-               moives = file.readlines()
-    movies = [movie.strop() for movie in movies]
-    return movies 
-  except FileNotFoundError:
-    return []
-def load_movies(horror):
-  try:
-    with open (f"{horror}.txt, "r") as file: 
-               moives = file.readlines()
-    movies = [movie.strop() for movie in movies]
-    return movies 
-  except FileNotFoundError:
-    return []
-def load_movies(comedy):
-  try:
-    with open (f"{comedy}.txt, "r") as file: 
-               moives = file.readlines()
-    movies = [movie.strop() for movie in movies]
-    return movies 
-  except FileNotFoundError:
-    return []
-
-#This is going to be where I put the personality test questions, but for now Im just going to keep it as 1 question
-movie_recommendations = {
-  "action": ["Die Hard", "Avengers", "John Wick"]
-  "comedy": ["The Hangover", "Ferris Bueller's Day Off"]
-  "romance": ["The Notebook", "Pretty Woman"]
-# There will be a file associated with each of the genres that contains a large list of movies and answers to questions
-}
 
 #Function to recommend movies based on the answer, for now just off of "what is ur fav movie"
 def recommend_movies(favorite_movie):
-  #This is a placeholder for now. The real recomendation logic I'll add based on how i decide to do the personality test
-  #for now it will just pick one from the same genre, for now just the list, later the file
-  movies = load_movies(fav_genre):
+  movies = load_movies(genre_rec):
     if movies:
       return random.choice(movies)
     else:
-      return ["Try another movie] #This is also a place holder, Im having the personality test as mutiple choice
+      return ["Try another movie or try typing it more simple. Ex. The Avengers (instead of specific avengers move)"] #This is also a place holder, Im having the personality test as mutiple choice
         #So something similar will be displayed when something other than an option is input
-#This is going to be in charg of the Reccomend button
-def recomment_movie(fav_genre):
-  if fav_genre in movie_recommendations:
-    return random.choice(movie_recommendations[fav_genre]
   
 def on_recommend():
+  favorite_actor = actor_entry.get()
   favorite_movie = movie_entry.get()
-  favorite_actor = entry_actor.get()
   fav_genre = genre_var.get()
-  recommendations = recommend_movies(favorite_movie)
-  messagebox.showinfo("Recommendations", "/n".join(recommendations))
+
+  genre_from_actor = None
+  actor_found = False
+  genres =['action', 'comedy', 'romance', 'horror']
+
+for genre in genres:
+  actors = load_movies(genre_actor)
+  if favorite_actor in actors:
+    genre_from_actor = genre
+    actor_found = True 
+    break
+
+if not actor_found:
+  genre_from_actor = fav_genre
+
+if favorite_movie: 
+  recommendation = recommend_movie(genre_from_actor)
+else:
+  recommendation = recommend_movie(fav_genre)
+
+messagebox.showinfo("Recommendations", recommendation)
 
 #Tkinter 
 root = tk.Tk()
@@ -80,10 +75,10 @@ tk.Label(root, text= "What genre of movies do you typically like?")
 entry_actor = tk.Entry(root)
 antry_actor.pack()
 
-tk.Label(root, text= "what if your favorite type of movie?")
+tk.Label(root, text= "what if your favorite genre of movie?")
 genre_var = tk.StringVar()
 genre_var.set("Select a genre")
-genres = [ #Instert genres here]
+genres = ['action', 'comedy', 'romance', 'horror']
 genre_menu = tk.OptionMenu(root, genre_var, *genres)
 genre_menu.pack()
 
